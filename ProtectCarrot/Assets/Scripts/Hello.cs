220ef9cd379ba54a3a7fd92cac14d92d284990cd
traLk <trackonyou@outlook.com>
2021-09-08 22:29:09 +0800

    SOMETHING

---
+++ ProtectCarrot/Assets/Scripts/Hello.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+    // Start is called before the first frame update
+    public void Start() {
+        Debug.Log("你好强啊陆哥！");
+    }
+
+    // Update is called once per frame
+    public void Update() {
+        Debug.LogWarning("我好垃圾！");
+    }
+}
